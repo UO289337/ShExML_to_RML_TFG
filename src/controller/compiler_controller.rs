68a2aa02e9e55d8bf4498ac8313cb18f7d9baf97
Ruben <UO289337@uniovi.es>
2025-03-22 14:13:14 +0100

    Creado modelo básico

--- src/controller/compiler_controller.rs
+++ src/controller/compiler_controller.rs
@@ -1,4 +1,5 @@
 use crate::view;
+use crate::model;
 
 pub fn run() {
     let file_content = match view::main_view::input_shexml_file() {
@@ -6,5 +7,12 @@ pub fn run() {
         Err(error) => panic!("Problem opening the file: {error:?}"),
     };
 
-    
+    let lexer_analysis_result = match model::lexer_analyzer::lexer(&mut file_content.as_str()) {
+        Ok(tokens) => {
+            for token in tokens {
+                println!("{:?}", token);
+            }
+        }
+        Err(err) => println!("Error: {:?}", err),
+    };
 }
--- src/model/lexer_analyzer.rs
+++ src/model/lexer_analyzer.rs
@@ -1,10 +1,55 @@
-use winnow;
+use winnow::error::{ContextError, ErrMode};
+use winnow::prelude::*;
+use winnow::token::{literal, take_while};
+use winnow::combinator::{alt, preceded};
 
-enum token_type {
-    // TODO: Poner tipos de tokens
+#[derive(Debug, PartialEq)]
+enum TokenType {
+    IDENT,
+    URI,
 }
 
-struct token {
-    lexeme: &str,
-    token_type: token_type,
+#[derive(Debug, PartialEq)]
+pub struct Token {
+    lexeme: String,
+    token_type: TokenType,
+}
+
+fn identifier(input: &mut &str) -> Result<Token, ErrMode<ContextError>> {
+    take_while(1.., |c: char| c.is_alphabetic() || c == '_')
+        .map(|s: &str| Token {
+            lexeme: s.to_string(), 
+            token_type: TokenType::IDENT,
+        })
+        .parse_next(input)
+}
+
+fn uri(input: &mut &str) -> Result<Token, ErrMode<ContextError>> {
+    preceded(
+        alt((literal("http://"), literal("https://"))), 
+        take_while(1.., |c: char| c.is_ascii()),  
+    )
+    .map(|s: &str| Token {
+        lexeme: s.to_string(),
+        token_type: TokenType::URI,
+    })
+    .parse_next(input)
+}
+
+pub fn lexer(input: &mut &str) -> Result<Vec<Token>, ErrMode<ContextError>> {
+    let mut tokens = Vec::new();
+
+    while !input.is_empty() {
+        let _: Result<&str, ErrMode<ContextError>> = take_while(1.., char::is_whitespace).parse_next(input); // Se ignoran los espacios
+        match alt((uri, identifier)).parse_next(input) {
+            Ok(token) => tokens.push(token),
+            Err(ErrMode::Backtrack(_)) => {
+                eprintln!("Error: Token no reconocido en '{}'", input);
+                return Err(ErrMode::Backtrack(ContextError::default()));
+            }
+            Err(e) => return Err(e),
+        }
+    }
+
+    Ok(tokens)
 }
