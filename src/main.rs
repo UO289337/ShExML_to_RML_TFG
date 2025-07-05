b7e12e70d99675f45db5732216c8416a4a8b43ff
Ruben <UO289337@uniovi.es>
2025-07-05 02:41:36 +0200

    Código para el experimento de los tiempos de ejecución

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 
 use shexml_to_rml_tfg::controller;
 
+// use std::time::Instant;
+
 fn main() {
+    // let start = Instant::now();
     controller::compiler_controller::run();
+    // let duration = start.elapsed();
+    // println!("Tiempo de ejecución: {:?}", duration);
 }
